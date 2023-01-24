cec01ad65c00c52a698b213e10a3b930d3aecf51
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-01-24 16:19:00 +0100

    [Add] scrapy exercice 03

--- workshop_scraping/workshop_scraping/spiders/ecommerce.py
+++ workshop_scraping/workshop_scraping/spiders/ecommerce.py
@@ -7,8 +7,28 @@ class EcommerceSpider(scrapy.Spider):
 
     def parse(self, response):
         titles = []
+        detail_link = []
+        description = []
+        price = []
+        image = []
+        rating = []
+        review = []
         for resp in response.xpath('.//a/@title'):
             data = { "data" : resp.get()}
             titles.append(resp.get())
             yield data
+        for resp in response.xpath('.//a/@title'):
+            data = { "data" : resp.get()}
+            detail_link.append(resp.get())
+            yield data
+        for resp in response.xpath('.//p/@description'):
+            data = { "data" : resp.get()}
+            description.append(resp.get())
+            yield data
         print("titles: ", titles)
+        print("detail link: ", detail_link)
+        print("description: ", description)
+        print("price: ", price)
+        print("image: ", image)
+        print("rating: ", rating)
+        print("review: ", review)
